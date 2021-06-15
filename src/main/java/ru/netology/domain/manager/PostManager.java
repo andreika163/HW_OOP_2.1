493666aa40e8879acaf9324e11bf6432ebc45136
Andrei Shishkin <shishkinam82@gmail.com>
2021-06-15 20:48:50 +0400

    add Post manager

---
+++ src/main/java/ru/netology/domain/manager/PostManager.java
@@ -0,0 +1,14 @@
+package ru.netology.domain.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] post;
+
+    public Post[] wallSearch (int owner_id, String domain, String query, boolean owners_only, int count, int offset) {
+        return null;
+    }
+
+    public void delete(int ownerId, int postId) {
+    }
+}
